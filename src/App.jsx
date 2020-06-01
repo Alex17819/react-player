dd9f91efa2570bca9860b3e75a9c008f544a49ee
Alex <a.b.25082001@gmail.com>
2020-06-01 16:23:06 +0300

    app component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './components/Main/Main'
+
+function App() {
+
+  return (
+    <div style={{ height:"100%", width: "100%"}}>
+        <Main />
+        
+    </div>
+  );
+}
+
+export default App;
