8e5ef51c09b668ebc69439e1005b5c57aa35f10b
Alex <a.b.25082001@gmail.com>
2020-06-05 19:21:15 +0300

    Add JSON file

---
+++ src/Assests/testJSON/test.jsx
@@ -0,0 +1,27 @@
+
+export default [
+    {
+        author: 'Xyaz',
+        name: 'Claws',
+        duration: '3: 10',
+        image: 'https://i1.sndcdn.com/artworks-9N87h575Vhym4mhy-z9KwDw-t500x500.jpg',
+        genre: 'Bass',
+        color: 'rgba(178, 65, 65)'
+    },
+    {
+        author: 'KRONO',
+        name: 'Dancin',
+        duration: '4: 12',
+        image: 'https://i1.sndcdn.com/artworks-000435082905-dixojj-t500x500.jpg',
+        genre: 'Electro/ House',
+        color: 'rgba(141, 42, 127)'
+    },
+    {
+        author: 'Rusty Hook',
+        name: 'All The Things She Said',
+        duration: '3: 02',
+        image: 'https://i1.sndcdn.com/artworks-000119841997-iun4wn-t500x500.jpg',
+        genre: 'Trap',
+        color: 'rgba(0, 0, 0)'
+    }
+]
